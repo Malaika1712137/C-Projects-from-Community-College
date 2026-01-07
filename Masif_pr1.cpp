e5cb5b9bcaa74605e036b43c0e62c1a0918c7761
Malaika Asif <malaikanoorasif@gmail.com>
2026-01-06 22:52:12 -0500

    Set project due date to 02/15/2024
    Updated due date for the project.

--- Masif_pr1.cpp
+++ Masif_pr1.cpp
@@ -3,7 +3,7 @@
  * Instructor:
  * Project<number>
  * Description: This is a beginner-friendly C++ console program that interacts with the user, asks for some inputs, and demonstrates basic math operations and division behavior in C++.
- * Due Date:
+ * Due Date: 02/15/2024
  * I pledge that I have completed the programming assignment independently.
    I have not copied the code from a student or any source.
    I have not given my code to any student.
